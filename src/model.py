8f45b61921a96b93a20cb0c0df99c7063fcda2e1
medonmol <gairola.abhijit.ctrl@gmail.com>
2021-09-27 16:54:42 +0530

    added a new model to models

--- src/model.py
+++ src/model.py
@@ -1,6 +1,5 @@
 import torch.nn as nn
 from torchvision.models import resnet18
-import torch
 
 
 class Model(nn.Module):
@@ -8,10 +7,7 @@ class Model(nn.Module):
         super().__init__()
         self.model = resnet18(pretrained=False, num_classes=10)
         self.model.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1, bias=False)
-        self.maxpool = nn.Identity()
+        self.model.maxpool = nn.Identity()
 
     def forward(self, x):
         return self.model(x)
-
-    def load_weights(self, model_path):
-        return self.model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
